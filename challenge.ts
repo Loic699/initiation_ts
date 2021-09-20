2200b63ef0ddbb89887298ea9240351135728883
Loic699 <loicpimet@icloud.com>
2021-09-20 10:21:17 +0200

    fisrt commit

---
+++ challenge.ts
@@ -0,0 +1,17 @@
+import { User } from "./User";
+
+// challenge.ts
+const prettyPrintWilder = (users: User []) => {
+    users.map((user) => {
+      console.log(`${user.name} is ${user.age} years old`);
+    });
+  };
+  
+  const wilders: User[] = [];
+  const user1: User = { name: "Pierre", age: 23, birthday: "10/02/1990"};
+  const user2: User = { name: "Paul", birthday: "10/02/1990", age: 18};
+  const user3: User = { name: "Jacques", age: 25, birthday: "10/02/1990" };
+  wilders.push(wilders[0]);
+  wilders.push(wilders[1]);
+  wilders.push(wilders[2]);
+  prettyPrintWilder(wilders);
